87ff7a237b8b30f779505198ff4bdfd88b50dcd9
Omer6546 <omer6546@gmail.com>
2021-11-10 03:47:47 +0200

    init

---
+++ Assets/Assets/Scripts/GameOverOnTrigger2D.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverOnTrigger2D : MonoBehaviour
+{
+    [Tooltip("Every object tagged with this tag will trigger game over")]
+    [SerializeField] string triggeringTag;
+    [SerializeField] static int life = 3;
+    [SerializeField] public GameObject[] fhearts;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == triggeringTag && enabled) {
+            Destroy(other.gameObject);
+            if (life >= 1)
+            {
+                life--;
+                this.gameObject.transform.position = new Vector3(0.11f, -6.74f, 0);
+
+            }
+            if (life < 1)
+            {
+                Debug.Log("Game over!");
+                Application.Quit();
+                #if UNITY_EDITOR
+                    UnityEditor.EditorApplication.isPlaying = false;
+                #endif
+            }
+
+        }
+    }
+    private void Start()
+    {
+        //hearts = new GameObject[life];
+    }
+    private void Update() {
+        /* Just to show the enabled checkbox in Editor */
+        
+    }
+
+}
